deea7f31cb3e347c41167c5aed0d67fa58d46af8
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 11:27:29 +0800

    [#1] using dummy hotel serializer

--- app/serializers/hotel_blueprint.rb
+++ app/serializers/hotel_blueprint.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class HotelBlueprint < Blueprinter::Base
-  identifier :identifier
+  identifier :identifier, name: :id
 
   # TODO: Add remaining attributes when data modeling is defined
   fields :name, :description, :destination_id
