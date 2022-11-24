bba74f9b67cc9591f61c7803685d377c1f637136
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 16:57:21 +0800

    [#17] updated blueprint for hotel to include all attributes

--- app/serializers/hotel_blueprint.rb
+++ app/serializers/hotel_blueprint.rb
@@ -4,5 +4,5 @@ class HotelBlueprint < Blueprinter::Base
   identifier :identifier, name: :id
 
   # TODO: Add remaining attributes when data modeling is defined
-  fields :name, :description, :destination_id
+  fields :name, :description, :destination_id, :latitude, :longitude, :address, :city, :country, :postal_code, :amenities_keywords, :booking_conditions
 end
