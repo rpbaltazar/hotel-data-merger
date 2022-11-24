0cf027535242d31f5ba6756d47d24544002b3c9f
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 13:13:58 +0800

    [#3] preparing specs for parsing array

--- spec/services/data_mappers/acme/hotel_spec.rb
+++ spec/services/data_mappers/acme/hotel_spec.rb
@@ -3,29 +3,31 @@
 require 'rails_helper'
 
 describe DataMappers::Acme::Hotel do
+  let(:raw_data) do
+    {
+      "Id": "iJhz",
+      "DestinationId": 5432,
+      "Name": "Beach Villas Singapore",
+      "Latitude": 1.264751,
+      "Longitude": 103.824006,
+      "Address": " 8 Sentosa Gateway, Beach Villas ",
+      "City": "Singapore",
+      "Country": "SG",
+      "PostalCode": "098269",
+      "Description": "  This 5 star hotel is located on the coastline of Singapore.",
+      "Facilities": [
+        "Pool",
+        "BusinessCenter",
+        "WiFi ",
+        "DryCleaning",
+        " Breakfast"
+      ]
+    }
+  end
+
   describe 'hotel_model' do
     it 'returns a hotel model from the attributes' do
-      data = {
-        "Id": "iJhz",
-        "DestinationId": 5432,
-        "Name": "Beach Villas Singapore",
-        "Latitude": 1.264751,
-        "Longitude": 103.824006,
-        "Address": " 8 Sentosa Gateway, Beach Villas ",
-        "City": "Singapore",
-        "Country": "SG",
-        "PostalCode": "098269",
-        "Description": "  This 5 star hotel is located on the coastline of Singapore.",
-        "Facilities": [
-          "Pool",
-          "BusinessCenter",
-          "WiFi ",
-          "DryCleaning",
-          " Breakfast"
-        ]
-      }.as_json
-
-      hotel_model = described_class.new(data).hotel_model
+      hotel_model = described_class.new(raw_data.as_json).hotel_model
       expect(hotel_model).to have_attributes(
         identifier: 'iJhz',
         destination_id: 5432,
@@ -48,5 +50,11 @@ describe DataMappers::Acme::Hotel do
   end
 
   describe 'self.from_array' do
+    it 'returns an array of hotels' do
+      data = 2.times.map { raw_data }.as_json
+      hotels = described_class.from_array(hotels_data: data)
+      expect(hotels[0].class).to eq ::Hotel
+      expect(hotels.map(&:identifier)).to match_array %w[iJhz iJhz]
+    end
   end
 end
