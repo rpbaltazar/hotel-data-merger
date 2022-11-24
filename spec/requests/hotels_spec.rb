1773484c79f6adf755501d709a2e1ca85b653089
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 11:02:52 +0800

    [#1] added very simple route test

---
+++ spec/requests/hotels_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+describe API::V1::HotelsController do
+  let(:controller_path) { '/api/v1/hotels' }
+
+  context 'index' do
+    let(:request_path) { "#{controller_path}/" }
+    let(:request) { get request_path, as: :json }
+
+    it 'returns an array of hotels' do
+      request
+
+      expect(response).to have_http_status(200)
+      data = JSON.parse(response.body)
+      expect(data).to eq []
+    end
+  end
+end
