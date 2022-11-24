aafb2aa6c051078d2948ac60866c16ac8a2d1386
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 11:34:19 +0800

    [#1] added basic yard documentation

--- app/services/hotel_finder.rb
+++ app/services/hotel_finder.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class HotelFinder
+  # Will call multiple service providers in order to find the information about the hotels
+  # filtered by the parameters sent
   #
+  # @param destination_id [Integer] destination id that we plan to match in the service provider
+  # @param hotel_ids [Array<Integer>] list of hotel ids that we plan to match in the service provider
+  # @return [Array<Hotel>] array of hotels that match the passed params.
   def self.call(destination_id: nil, hotel_ids: [])
     # TODO: ensure that either is set once we support filtering
     new(destination_id, hotel_ids).call
