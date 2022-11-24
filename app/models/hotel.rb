8d28ad92ef7c50e5ec49e8ba363c785861e72d19
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 11:19:14 +0800

    [#1] added skeleton for hotel finder and hotel

---
+++ app/models/hotel.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class Hotel
+  attr_accessor :id, :identifier, :name, :latitude, :longitude, :address, :city, :country, :destination_id,
+                :amenities_keywords, :booking_conditions, :images
+end
