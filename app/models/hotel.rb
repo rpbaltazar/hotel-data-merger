f2ca2215532e6c0f11c5d64694c1861bc3e675f3
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 16:56:30 +0800

    [#17] added mergeable attributes

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,5 +2,7 @@
 
 class Hotel
   attr_accessor :id, :identifier, :name, :description, :latitude, :longitude, :address, :city, :country,
-              :postal_code, :destination_id, :amenities_keywords, :booking_conditions, :images
+                :postal_code, :destination_id, :amenities_keywords, :booking_conditions, :images
+
+  MERGEABLE_ATTRIBUTES = %i[description address city country postal_code booking_conditions].freeze
 end
