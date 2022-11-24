ea4400640a72e5745f6e9c651d09259cfd6e099b
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 11:19:25 +0800

    [#1] using hotel finder in controller

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -4,6 +4,9 @@ module API
   module V1
     class HotelsController < ApplicationController
       def index
+        hotels = ::HotelFinder.call(destination_id: nil, hotel_ids: [])
+        # TODO: invoke api call to different service providers
+        # TODO: return rendered matched hotels
         render json: []
       end
     end
