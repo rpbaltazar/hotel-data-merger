8c7ee5ffd59a6dd182b31f75e121c699e60d4301
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-24 11:27:29 +0800

    [#1] using dummy hotel serializer

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -4,10 +4,9 @@ module API
   module V1
     class HotelsController < ApplicationController
       def index
-        hotels = ::HotelFinder.call(destination_id: nil, hotel_ids: [])
         # TODO: invoke api call to different service providers
-        # TODO: return rendered matched hotels
-        render json: []
+        hotels = ::HotelFinder.call(destination_id: nil, hotel_ids: [])
+        render json: HotelBlueprint.render(hotels)
       end
     end
   end
