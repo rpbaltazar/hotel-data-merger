1b524129cfae1e9588660c3a01305ca9daa37189
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-25 12:57:02 +0800

    [#16] make flow work

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
-class Hotel
-  attr_accessor :id, :identifier, :name, :description, :latitude, :longitude, :address, :city, :country,
-                :postal_code, :destination_id, :amenities_keywords, :booking_conditions, :images
+class Hotel < ApplicationRecord
+  # attr_accessor :id, :identifier, :name, :description, :latitude, :longitude, :address, :city, :country,
+  #               :postal_code, :destination_id, :amenities_keywords, :booking_conditions, :images
 
-  MERGEABLE_ATTRIBUTES = %i[description address city country postal_code booking_conditions].freeze
+  # MERGEABLE_ATTRIBUTES = %i[description address city country postal_code booking_conditions].freeze
+  MERGEABLE_ATTRIBUTES = %i[description address city country].freeze
 end
