3742c3f6daf2b31251318c9d5e80ee2cd173c450
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-25 13:24:20 +0800

    [#16] added hotel raw data table

---
+++ db/migrate/20221125051624_create_hotel_raw_data.rb
@@ -0,0 +1,21 @@
+class CreateHotelRawData < ActiveRecord::Migration[6.1]
+  def change
+    create_table :hotel_raw_data do |t|
+      t.string :source
+      t.string :identifier, index: true
+      t.integer :destination_id
+      t.string :name
+      t.decimal :latitude, { precision: 10, scale: 6 }
+      t.decimal :longitude, { precision: 10, scale: 6 }
+      t.string :address
+      t.string :postal_code
+      t.string :country
+      t.string :description
+      t.jsonb :images
+      t.jsonb :amenities
+      t.string :booking_conditions, array: true
+
+      t.timestamps
+    end
+  end
+end
