47e50205e8ab5ecb90f44ea364d76909b850835a
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-25 15:29:18 +0800

    [#2] added filtering validation spec

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -4,8 +4,25 @@ module API
   module V1
     class HotelsController < ApplicationController
       def index
-        hotels = ::HotelFinder.call(destination_id: nil, hotel_ids: [])
+        if invalid_filtering_params
+          return render json: { message: 'You must filter by either destination_id or hotel_ids' },
+                        status: :unprocessable_entity
+        end
+
+        hotels = ::HotelFinder.call(destination_id: index_params[:destination_id], hotel_ids: index_params[:hotel_ids])
         render json: HotelBlueprint.render(hotels)
+      end
+
+      private
+
+      def index_params
+        params.permit(:destination_id, hotel_ids: [])
+      end
+
+      # NOTE: Either destination id or hotel ids must be set but not both at the same time
+      def invalid_filtering_params
+        (index_params[:destination_id].present? && index_params[:hotel_ids].present?) ||
+          (index_params[:destination_id].blank? && index_params[:hotel_ids].blank?)
       end
     end
   end
--- spec/requests/hotels_spec.rb
+++ spec/requests/hotels_spec.rb
@@ -8,17 +8,56 @@ describe API::V1::HotelsController do
   context 'index' do
     let(:request_path) { "#{controller_path}/" }
     let(:request) { get request_path, as: :json }
+    let(:hotels) { FactoryBot.create_list :hotel, 3 }
 
     before do
-      FactoryBot.create_list :hotel, 3
+      hotels
     end
 
-    it 'returns an array of hotels' do
-      request
+    describe 'when no filtering params are sent' do
+      it 'returns an error with a meaningful message' do
+        request
 
-      expect(response).to have_http_status(200)
-      data = JSON.parse(response.body)
-      expect(data.count).to eq 3
+        expect(response).to have_http_status(422)
+        data = JSON.parse(response.body)
+        expect(data['message']).to eq 'You must filter by either destination_id or hotel_ids'
+      end
+    end
+
+    describe 'when both filtering options are sent' do
+      let(:request_path) { "#{controller_path}/?destination_id=123&hotel_ids[]=12&hotel_ids[]=13" }
+
+      it 'returns an array of hotels' do
+        request
+
+        expect(response).to have_http_status(422)
+        data = JSON.parse(response.body)
+        expect(data['message']).to eq 'You must filter by either destination_id or hotel_ids'
+      end
+    end
+
+    describe 'when filtering by hotel ids' do
+      let(:request_path) { "#{controller_path}/?hotel_ids[]=#{hotels.first.identifier}&hotel_ids[]=#{hotels.second.identifier}" }
+
+      it 'returns an array of hotels' do
+        request
+
+        expect(response).to have_http_status(200)
+        data = JSON.parse(response.body)
+        expect(data.count).to eq 2
+      end
+    end
+
+    describe 'when filtering by destination id' do
+      let(:request_path) { "#{controller_path}/?destination_id=111" }
+
+      it 'returns an array of hotels' do
+        request
+
+        expect(response).to have_http_status(200)
+        data = JSON.parse(response.body)
+        expect(data.count).to eq 3
+      end
     end
   end
 end
