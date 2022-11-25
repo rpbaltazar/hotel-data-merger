1874d3a1b3516bc2041b756cbf9ef96fcfd0aed5
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-25 14:33:20 +0800

    [#16] removed old poro code

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class Hotel < ApplicationRecord
-  # attr_accessor :id, :identifier, :name, :description, :latitude, :longitude, :address, :city, :country,
-  #               :postal_code, :destination_id, :amenities_keywords, :booking_conditions, :images
-
-  # MERGEABLE_ATTRIBUTES = %i[description address city country postal_code booking_conditions].freeze
-  MERGEABLE_ATTRIBUTES = %i[description address city country].freeze
 end
