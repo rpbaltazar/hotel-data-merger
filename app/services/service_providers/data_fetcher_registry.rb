a0c8e418deb40fac7db1f171d2d2085c3e58e343
Rui Baltazar <rui.p.baltazar@gmail.com>
2022-11-25 14:30:54 +0800

    [#16] removed unnecessary method

--- app/services/service_providers/data_fetcher_registry.rb
+++ app/services/service_providers/data_fetcher_registry.rb
@@ -8,25 +8,5 @@ module ServiceProviders
       paperflies: ServiceProviders::Paperflies::DataFetcher,
       patagonia: ServiceProviders::Patagonia::DataFetcher
     }.freeze
-
-    # TODO: This should be a service of its own
-    def self.merge_hotel_info(cached_hotel, new_info)
-      ::Hotel::MERGEABLE_ATTRIBUTES.each do |attribute|
-        current_attribute_value = cached_hotel.send(attribute)
-        new_attribute_value = new_info.send(attribute)
-
-        if current_attribute_value.blank? && new_attribute_value.present?
-          cached_hotel.send("#{attribute}=", new_attribute_value)
-        end
-
-        if current_attribute_value.present? && new_attribute_value.present?
-          if current_attribute_value.length < new_attribute_value.length
-            cached_hotel.send("#{attribute}=", new_info.send(attribute))
-          end
-        end
-      end
-
-      cached_hotel
-    end
   end
 end
